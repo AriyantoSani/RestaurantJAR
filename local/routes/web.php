b673c482bb5926ce0f2ab78a2a50c3b14f75377c
Ariyanto Sani <38694623+AriyantoSani@users.noreply.github.com>
2020-01-05 02:26:08 +0700

    commit

--- local/routes/web.php
+++ local/routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+
+Route::get('/home','homepageController@index');
+Route::get('/','homepageController@index');
+
+// Route::get('/', 'loginController@index');
+// Route::post('/', 'loginController@login');
+// Route::get('/login', 'loginController@index');
+// Route::post('/login', 'loginController@login');
+// Route::get('/createaccount', 'loginController@createaccountIndex');
+// Route::post('/createaccount', 'loginController@createaccount');
+// // });
+// Route::group(['middleware' => 'usersession'], function () {
+// Route::get('/', 'homeController@Index');
+// Route::get('/home', 'homeController@Index');
+// Route::get('/logout', 'homeController@logout');
+// Route::get('/topup', 'homeController@topupindex');
+// Route::post('/topup', 'homeController@topup');
+// });
