0743f5348dd6e73666261f1861672e620653a263
Ariyanto Sani <ariyantosani555@gmail.com>
2020-01-07 12:23:57 +0700

    a

--- local/resources/views/menu.blade.php
+++ local/resources/views/menu.blade.php
@@ -76,7 +76,7 @@
         </div>
         <?php if ($status == 1) {
         ?>
-        <a href="{{ asset('menu/updateOrder/'.$id) }} ">
+        <a href="{{ asset('table/updateOrder/'.$id) }} ">
             <button class="btn btn-primary">Update</button>
         </a>
         <a href="{{asset('/orderlist/'.$id)}}"> <button type="button" class="btn btn-primary">Order List</button></a>
